3faa93c045754b33174f9dc113e7638bda8dd3e7
exoplayerjib <orelji@post.bgu.ac.il>
2025-06-19 01:07:35 +0300

    Added toString and equals ovverides

--- src/Game/Tiles/Tile.java
+++ src/Game/Tiles/Tile.java
@@ -17,4 +17,20 @@ public abstract class Tile {
     public double range(Tile other){
         return Math.sqrt(Math.pow(this.position.getX() - other.position.getX(), 2) + Math.pow(this.position.getY() - other.position.getY(),2));
     }
+
+    @Override
+    public String toString(){
+        return ""+this.character;
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if(other instanceof Tile that){
+            return that.position.equals(this.position) &&
+                    that.character == this.character &&
+                    that.walkable == this.walkable;
+        }
+        return false;
+    }
+
 }
