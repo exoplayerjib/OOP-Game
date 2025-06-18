97d2e800863623da360d1045120edb4da7214404
exoplayerjib <orelji@post.bgu.ac.il>
2025-06-19 01:17:27 +0300

    Revised the initiation of Position and changed character -> tile

--- src/Game/Tiles/Tile.java
+++ src/Game/Tiles/Tile.java
@@ -4,14 +4,13 @@ import Game.Utils.Position;
 
 public abstract class Tile {
 
-    protected char character;
+    protected char tile;
     protected Position position;
     protected boolean walkable;
 
     public Tile(char sym, int x, int y){
-        this.character = sym;
-        this.position.setX(x);
-        this.position.setY(y);
+        this.tile = sym;
+        position = new Position(x,y);
     }
 
     public double range(Tile other){
@@ -20,14 +19,14 @@ public abstract class Tile {
 
     @Override
     public String toString(){
-        return ""+this.character;
+        return ""+this.tile;
     }
 
     @Override
     public boolean equals(Object other){
         if(other instanceof Tile that){
             return that.position.equals(this.position) &&
-                    that.character == this.character &&
+                    that.tile == this.tile &&
                     that.walkable == this.walkable;
         }
         return false;
