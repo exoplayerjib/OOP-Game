cc2c9338a1c5ab7fd0854c53b14f5cc4556f4aca
exoplayerjib <orelji@post.bgu.ac.il>
2025-06-19 12:13:55 +0300

    updated Position toString

--- src/Game/Utils/Position.java
+++ src/Game/Utils/Position.java
@@ -32,6 +32,6 @@ public class Position {
 
     @Override
     public String toString() {
-        return  "My position is: (" + x + "," + y + ")";
+        return  "(" + x + "," + y + ")";
     }
 }
