93502fd64c1ab731cf6d42f2832e9018e94a8ce5
exoplayerjib <orelji@post.bgu.ac.il>
2025-06-23 19:18:04 +0300

    NOT-IMPLEMENTED, just made the backbone for the board class

--- src/Game/Board/Board.java
+++ src/Game/Board/Board.java
@@ -4,16 +4,33 @@ import Game.Callbacks.MessageCallback;
 import Game.Tiles.Tile;
 import Game.Tiles.Units.Enemies.Enemy;
 import Game.Tiles.Units.Players.Player;
+import Game.Utils.Position;
 
 import java.util.List;
 
-public class Board {
-    private final int rows;
-    private final int cols;
-    private final Tile[][] board;
-    private final List<Enemy> enemies;
-    private Player player;
-    private final MessageCallback messageCallback;
+
+/// FIXME make non-abstract
+public abstract class Board {
+//    private final int rows;
+//    private final int cols;
+//    private final Tile[][] board;
+//    private final List<Enemy> enemies;
+//    private Player player;
+//    private final MessageCallback messageCallback;
     //TODO ALL!!!!
+
+    public Board(){}
+
+    public abstract void swapPositions(Tile tile1, Tile tile2);
+
+    public abstract List<Enemy> getEnemies();
+
+    public abstract List<Enemy> getLivingEnemies();
+
+    public abstract Player getPlayer();
+
+    public abstract void setTile(Position position, Tile tile);
+
+
 
 }
