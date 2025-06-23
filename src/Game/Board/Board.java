c02d73cb814945227a3971ea67790f7ab2ac3f84
exoplayerjib <orelji@post.bgu.ac.il>
2025-06-23 17:17:27 +0300

    Created class Board need to implement

---
+++ src/Game/Board/Board.java
@@ -0,0 +1,19 @@
+package Game.Board;
+
+import Game.Callbacks.MessageCallback;
+import Game.Tiles.Tile;
+import Game.Tiles.Units.Enemies.Enemy;
+import Game.Tiles.Units.Players.Player;
+
+import java.util.List;
+
+public class Board {
+    private final int rows;
+    private final int cols;
+    private final Tile[][] board;
+    private final List<Enemy> enemies;
+    private Player player;
+    private final MessageCallback messageCallback;
+    //TODO ALL!!!!
+
+}
