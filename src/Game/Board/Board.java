030e45536f9a1ee8d86346d2e9ebc7d4b7914ce2
Orel Jibli <154694868+exoplayerjib@users.noreply.github.com>
2025-06-27 15:24:16 +0300

    Created Board Class

--- src/Game/Board/Board.java
+++ src/Game/Board/Board.java
@@ -1,36 +1,106 @@
 package Game.Board;
 
 import Game.Callbacks.MessageCallback;
+import Game.Tiles.BoardParts.Empty;
 import Game.Tiles.Tile;
 import Game.Tiles.Units.Enemies.Enemy;
 import Game.Tiles.Units.Players.Player;
 import Game.Utils.Position;
+import View.InputQuery;
+import View.Parser.TileFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 
-/// FIXME make non-abstract
-public abstract class Board {
-//    private final int rows;
-//    private final int cols;
-//    private final Tile[][] board;
-//    private final List<Enemy> enemies;
-//    private Player player;
-//    private final MessageCallback messageCallback;
-    //TODO ALL!!!!
+public  class Board {
+    private final int rows;
+    private final int cols;
+    private final Tile[][] board;
+    private final List<Enemy> enemies;
+    private Player player;
+    private final MessageCallback messageCallback;
+    private final InputQuery inputQuery;
+    private final TileFactory tileFactory = new TileFactory();
 
-    public Board(){}
+    public Board(Path file, Player player, MessageCallback messageCallback, InputQuery inputQuery) throws IOException {
+        List<String> lines = Files.readAllLines(file);
+        rows = lines.size();
+        cols = lines.get(0).length();
+        board = new Tile[cols][rows];
+        enemies = new ArrayList<>();
+        this.player = player;
+        this.inputQuery = inputQuery;
+        this.messageCallback = messageCallback;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                char sym = lines.get(i).charAt(j);
+                Position position = new Position(i, j);
+                board[i][j] = createTile(sym, position); ///TODO no checks might cause errors
+            }
+        }
+    }
 
-    public abstract void swapPositions(Tile tile1, Tile tile2);
+    private Tile createTile(char sym, Position position){
 
-    public abstract List<Enemy> getEnemies();
+        if (sym == player.getTile()){
+            return player.init(position,messageCallback,this, inputQuery);
+        }
 
-    public abstract List<Enemy> getLivingEnemies();
+        if (tileFactory.getBoardPartSymbols().contains(sym)){
+            return tileFactory.createBoardPart(sym,position);
+        }
+        if (tileFactory.getEnemySymbols().contains(sym)){
+            return tileFactory.createEnemy(sym,position,messageCallback,this);
+        }
+        else {
+            System.out.println("Error while loading file, no such tile: " + sym + " at position: " + position.toString() + "!");
+            System.exit(-1);
+            return null;
+        }
+    }
 
-    public abstract Player getPlayer();
+    public boolean inBounds(Position position){
+        return position.getX() >= 0 && position.getX() < cols && position.getY() >= 0 && position.getY() < rows;
+    }
 
-    public abstract void setTile(Position position, Tile tile);
+    public Tile getTile(Position position){
+        if (inBounds(position))
+            return board[position.getX()][position.getY()];
+        else
+            return null;
+    }
 
+    public void setTile(Position position, Tile tile){
+        if (inBounds(position))
+            board[position.getX()][position.getY()] = tile;
+    }
 
+    public void swapPositions(Tile operator, Tile operatee){
+        Position p1 = operator.getPosition();
+        Position p2 = operatee.getPosition();
+        operator.setPosition(p2);
+        operatee.setPosition(p1);
+        setTile(p2,operator);
+        setTile(p1,operatee);
+    }
 
+    public Tile removeEnemy(Enemy enemy){
+        enemies.remove(enemy);
+        Empty empty = new Empty();
+        empty.init(enemy.getPosition());
+        setTile(enemy.getPosition(), empty);
+        return empty;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public List<Enemy> getEnemies(){
+        return enemies;
+    }
 }
