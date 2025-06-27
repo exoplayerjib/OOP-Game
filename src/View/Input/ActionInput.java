e57ea31eddb77b2ba1c4eceb6de57146cd6be67a
Orel Jibli <154694868+exoplayerjib@users.noreply.github.com>
2025-06-28 00:17:47 +0300

    Added `ActionInput` class for player action handling
    - Implemented `ActionInput` to manage user input for player actions via a `Scanner`.
    - Mapped valid character inputs to corresponding player `Actions`.
    - Included input validation with retry mechanism for incorrect inputs.

---
+++ src/View/Input/ActionInput.java
@@ -0,0 +1,38 @@
+package View.Input;
+
+import Game.Tiles.Units.Players.Player;
+import Game.Tiles.Units.Players.Player.Actions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class ActionInput implements InputQuery{
+    private final Map<Character,Actions> validInput = new HashMap<>(){{
+        put('w',Actions.UP);
+        put('a',Actions.LEFT);
+        put('s',Actions.DOWN);
+        put('d',Actions.RIGHT);
+        put('e',Actions.CAST);
+        put('q',Actions.STAY);
+    }};
+
+    private final Scanner scanner;
+
+    public ActionInput(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    public Actions getInput(){
+        while (true){
+            char input = scanner.next().charAt(0);
+            if (validInput.containsKey(input)){
+                return validInput.get(input);
+            }
+            else {
+                System.out.println("Invalid input, please try again!");
+            }
+        }
+    }
+}
